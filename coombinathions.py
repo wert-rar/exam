1edb87651a2fc8554d62f9b22805cfe3ed5c2f0e
wert-rar <72702040+wert-rar@users.noreply.github.com>
2021-02-20 18:38:29 +0300

    Add files via upload

---
+++ coombinathions.py
@@ -0,0 +1,269 @@
+class CombSolver:
+
+    def __init__(self, start: int, end: int, pluses=None, multipliers=None, powers=None, prev=False):
+        self.start = start
+        self.end = end + 1
+        self.all_ways = [1] * (end - start + 1)
+        # multiply by
+        self.multipliers = multipliers
+        self.pluses = pluses
+        self.powers = powers
+        # get func for calculating ways
+        self.calculate_f = self.get_final_function(prev)
+
+    def debug_log(self):
+        print('Number      Count of ways')
+        for i in range(self.end - self.start):
+            print(i + self.start, '       ', self.all_ways[i])
+
+    # will be override in children's--------
+    def get_ways_from_pluses(self, n):
+        """
+        for example:
+            plus is 2
+            plus2 is 3
+            plus3 is 10
+            number is 10
+        1) 10 - 2 = 8  so we need a count of ways to 8
+        2) 10 - 3 = 7  so we need a count of ways to 7
+        3)10 - 10 = 0   so we cant get a number if ways
+        """
+        pass
+
+    def get_ways_from_dels(self, n):
+        """
+        for example:
+            del is 4
+            number is 24
+            24 / 4 = 6 so we need a count of ways to 6
+        """
+
+    def get_ways_from_power(self, n):
+        """
+        for example:
+             del is 4
+             number is 24
+             24 / 4 = 6 so we need a count of ways to 6
+        """
+
+    # check is the current number a (x + x-1)
+    def get_ways_from_prev(self, n):
+
+        if n % 2 == 0:
+            return 0
+        x = (n + 1) / 2
+        if x in range(self.start, n + 1):
+            return self.all_ways[int(x) - 1] + self.all_ways[int(x)]
+
+    # ---------------------------------------
+
+    # combines checks for factors,summands and squares,
+    # eliminates the action if the list is empty
+    def get_final_function(self, prev):
+        if prev:
+            if self.pluses is not None:
+                if self.multipliers is not None:
+                    if self.powers is not None:
+                        return lambda n: self.get_ways_from_pluses(n) + \
+                                         self.get_ways_from_dels(n) + \
+                                         self.get_ways_from_power(n) + \
+                                         self.get_ways_from_prev(n)
+                    else:
+                        return lambda n: self.get_ways_from_pluses(n) + \
+                                         self.get_ways_from_dels(n) + \
+                                         self.get_ways_from_prev(n)
+                else:
+                    if self.powers is not None:
+                        return lambda n: self.get_ways_from_pluses(n) + \
+                                         self.get_ways_from_power(n) + \
+                                         self.get_ways_from_prev(n)
+                    else:
+                        return lambda n: self.get_ways_from_pluses(n) + \
+                                         self.get_ways_from_prev(n)
+
+            else:
+                if self.multipliers is not None:
+                    if self.powers is not None:
+                        return lambda n: self.get_ways_from_dels(n) + \
+                                         self.get_ways_from_power(n) + \
+                                         self.get_ways_from_prev(n)
+                    else:
+                        return lambda n: self.get_ways_from_dels(n) + \
+                                         self.get_ways_from_prev(n)
+                else:
+                    if self.powers is not None:
+                        return lambda n: self.get_ways_from_power(n) + \
+                                         self.get_ways_from_prev(n)
+                    else:
+                        return lambda n: self.get_ways_from_prev(n)
+        else:
+            if self.pluses is not None:
+                if self.multipliers is not None:
+                    if self.powers is not None:
+                        return lambda n: self.get_ways_from_pluses(n) + \
+                                         self.get_ways_from_dels(n) + \
+                                         self.get_ways_from_power(n)
+                    else:
+                        return lambda n: self.get_ways_from_pluses(n) + \
+                                         self.get_ways_from_dels(n)
+                else:
+                    if self.powers is not None:
+                        return lambda n: self.get_ways_from_pluses(n) + \
+                                         self.get_ways_from_power(n)
+                    else:
+                        return lambda n: self.get_ways_from_pluses(n)
+
+            else:
+                if self.multipliers is not None:
+                    if self.powers is not None:
+                        return lambda n: self.get_ways_from_dels(n) + \
+                                         self.get_ways_from_power(n)
+                    else:
+                        return lambda n: self.get_ways_from_dels(n)
+                else:
+                    if self.powers is not None:
+                        return lambda n: self.get_ways_from_power(n)
+                    else:
+                        raise AttributeError("can't solve task if robot don't have any actions")
+
+
+class CombSolverWithoutTr(CombSolver):
+    def __init__(self, start: int, end: int, pluses=None, multipliers=None, powers=None, prev=False):
+        super().__init__(start, end, pluses, multipliers, powers, prev)
+
+    def get_ways_from_pluses(self, n):
+        ways = 0
+        for p in self.pluses:
+            if n - p >= self.start:
+                ways += self.all_ways[n - p - self.start]
+
+        return ways
+
+    def get_ways_from_dels(self, n):
+        ways = 0
+        for m in self.multipliers:
+            if n % m == 0 and n // m >= self.start:
+                ways += self.all_ways[n // m - self.start]
+        return ways
+
+    def get_ways_from_power(self, n):
+        ways = 0
+        for m in self.multipliers:
+            x = n ** (1 / m)
+            # if x exists in cache, it also will be integer
+            if x in range(self.start, n + 1):
+                ways += self.all_ways[int(x) - self.start]
+        return ways
+
+    # calculate number of ways for every number
+    def get_comb_without_trajectory(self, debug=False):
+        # count of methods to get this number
+        for n in range(self.start + 1, self.end):
+            self.all_ways[n - self.start] = self.calculate_f(n)
+
+        print(f'Answer -  {self.all_ways[-1]} \n')
+
+        # print table of ways if user want
+        if debug:
+            self.debug_log()
+
+
+class CombSolverTr(CombSolver):
+    """
+    This class solve task with Trajectory Points and  not Trajectory Points
+    :tr - points in trajectory
+
+    :not_tr - points out of trajectory
+
+    """
+    def __init__(self, start: int, end: int, tr: [], not_tr: [], pluses=None, multipliers=None, powers=None):
+
+        if len(tr) == 0:
+            raise AttributeError('numbers in trajectory is empty. \nif you don\'t need them \nuse class '
+                                 'CombSolverWithoutTr ')
+
+        super().__init__(start, end, pluses, multipliers, powers)
+        self.not_tr = not_tr
+        self.tr = tr
+        self.now_tr = start
+
+    def get_ways_from_pluses(self, n):
+        ways = 0
+        for p in self.pluses:
+            #  number must be more than trajectory and don't be in not_trajectory
+            if (n - p >= self.now_tr) and (n - p not in self.not_tr):
+                ways += self.all_ways[n - p - self.start]
+        return ways
+
+    def get_ways_from_dels(self, n):
+        ways = 0
+        for m in self.multipliers:
+            #  number must be more than trajectory and don't be in not_trajectory
+            if (n % m == 0) and \
+              (n // m >= self.now_tr) and \
+              (n // m not in self.not_tr):
+
+                ways += self.all_ways[n // m - self.start]
+        return ways
+
+    def get_ways_from_power(self, n):
+        ways = 0
+        for m in self.multipliers:
+            x = n ** (1 / m)
+            #  number must be more than trajectory and don't be in not_trajectory
+            if (x in range(self.now_tr, n + 1)) \
+               and (x not in self.not_tr):
+
+                ways += self.all_ways[int(x) - self.start]
+
+        return ways
+
+    # calculate ways from
+    # one trajectory point to another
+    def fill_range(self, next_tr):
+        for n in range(self.now_tr + 1, next_tr):
+            self.all_ways[n - self.start] = self.calculate_f(n)
+
+    # make all steps for one range between trajectory points
+    def do_one_tr(self, tr):
+        self.fill_range(tr)
+        # calculate ways of trajectory point
+        # !!! always do it BEFORE changing trajectory!
+        self.all_ways[tr - self.start] = self.calculate_f(tr)
+        self.now_tr = tr
+
+    # split range of values to ranges and calculate ways for each
+    def get_comb_with_trajectory(self, debug=False):
+        # fill not trajectories with null
+        for tr in self.not_tr:
+            self.all_ways[tr - self.start] = 0
+
+        # start range always is (start, first trajectory)
+        self.do_one_tr(self.tr[0])
+
+        # main part
+        for tr in range(1, len(self.tr)):
+            self.do_one_tr(self.tr[tr])
+
+        # last range always is (last trajectory; end)
+        self.fill_range(self.end)
+
+        # print numbers of ways to last number
+        print(f'Answer -  {self.all_ways[-1]} \n')
+        # print table if user want
+        if debug:
+            self.debug_log()
+
+
+def main():
+    print('Task Without Trajectory')
+    c = CombSolverWithoutTr(1, 10, [1,2], [])
+    c.get_comb_without_trajectory(True)
+    print()
+    print('Task With Trajectory')
+    ct = CombSolverTr(1, 10, [5], [], [1, 2])
+    ct.get_comb_with_trajectory(True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ logic.py
@@ -0,0 +1,266 @@
+# analog for -> operator
+arrow = lambda A, B: int(not (A > B))
+
+
+# LTS -- LogicTableSolver
+
+class BaseLTS:
+
+    def __init__(self, number_of_x):
+        self.n = number_of_x
+
+    @staticmethod
+    def create_column(n, length):
+        # count of zero and count of ones in one pare
+        zero, one = [], []
+        row = []
+
+        count = 2 ** (n - 1)
+        # fill the pare with require count of numbers
+        for i in range(count):
+            zero.append(0)
+            one.append(1)
+        # count of pares of zeros and ones
+        count_of_no = length // (count * 2)
+
+        for i in range(count_of_no):
+            row.extend(zero)
+            row.extend(one)
+
+        return row
+
+    @staticmethod
+    def create_var_table(n):
+        table = []
+        len_ = 2 ** n
+        for i in range(n, 0, -1):
+            table.append(BaseLTS.create_column(i, len_))
+        return table
+
+    @staticmethod
+    def create_title(n):
+        form = ''
+        for i in range(n):
+            form += f'x{i + 1}  '
+        form += 'F'
+        return form
+
+    @staticmethod
+    def print_table(n, table):
+        print(BaseLTS.create_title(n))
+        form = '{}   ' * n
+        for i in range(2 ** n):
+            table_row = [t[i] for t in table]
+            print(
+                form.format(*table_row))
+
+    @staticmethod
+    def print_log_table(n, table, fs):
+
+        print(BaseLTS.create_title(n))
+        form = '{}   ' * (n + 1)
+        # if we have list of answers we only write answers
+        if isinstance(fs, list):
+            for i in range(2 ** n):
+                table_row = [t[i] for t in table]
+                print(form.format(*table_row, fs[i]))
+        # else we calculate and write answer
+        else:
+            for i in range(2 ** n):
+                table_row = [t[i] for t in table]
+                print(form.format(*table_row, fs(*table_row)))
+
+    def solve(self):
+        table = self.create_var_table(self.n)
+        print("gen table with all possible all_ways")
+        self.print_table(self.n, table)
+
+
+class OneFormulaLTS(BaseLTS):
+    def __init__(self, number_of_x, formula):
+        super().__init__(number_of_x)
+        self.formula = formula
+        self.table = self.create_var_table(number_of_x)
+
+    def create_log_table(self):
+        f_column = []
+        for i in range(2 ** self.n):
+            table_row = [t[i] for t in self.table]
+            f_column.append(int(
+                self.formula(*table_row)
+            ))
+
+        return f_column
+
+    def solve(self):
+        print("gen all possible variations of formula")
+        self.print_log_table(
+            self.n,
+            self.table,
+            self.create_log_table(),
+        )
+
+
+class LotFormulaLTS(BaseLTS):
+    def __init__(self, number_of_x, formulas, pattern=None, giving_table=None):
+        super().__init__(number_of_x)
+        self.giving_table = giving_table
+        self.formulas = formulas
+        self.pattern = pattern
+
+    def normalize_giving_table(self):
+        # create all possible variations of arguments
+        table = self.create_var_table(self.n)
+        compared_table = []
+
+        for row in self.giving_table:
+            for i in range(2 ** self.n):
+                table_row = [t[i] for t in table]
+
+                # if row already in table we don't need check it again
+                # if row  have less ones or less zeros, it can't be in compare_table
+                if table_row not in compared_table and \
+                        table_row.count(1) >= row.count(1) and \
+                        table_row.count(0) >= row.count(0):
+                    compared_table.append(table_row)
+
+        self.giving_table = compared_table
+
+    def compare_formulas(self):
+
+        ind = None
+        correct = []
+
+        if len(self.pattern) != len(self.giving_table[0]):
+            print("Error! \nnumber of answers != rows of table")
+            print(len(self.pattern))
+            print(len(self.giving_table))
+            return
+
+        # Get result for each formula and compare with expected answer
+        for f in self.formulas:
+
+            answer = []
+            # collect answers of formula
+            for i in range(len(self.giving_table)):
+                table_row = self.giving_table[i]
+                print(table_row, f(*table_row))
+                answer.append(int(f(*table_row)))
+            print('----------')
+            # get correct answer
+            if answer == self.pattern:
+                ind = self.formulas.index(f)
+                correct = answer
+        print(f'\nthe right formula is {ind} \nanswers: {correct}')
+
+    def solve(self):
+
+        if self.giving_table is None and self.formulas is None:
+            print('cant find right answer in Nothing')
+
+        elif self.formulas is None:
+            print('cant build answer without formulas')
+
+        elif self.giving_table is None:
+            print('Automatically create Logic Table\n')
+            print('Start solving')
+
+            self.giving_table = self.create_var_table(self.n)
+            self.compare_formulas()
+        else:
+            print('Start solving')
+
+            # self.normalize_giving_table()
+            self.compare_formulas()
+
+
+class FinderLTS(OneFormulaLTS):
+    def __init__(self, number_of_x, f, giving_table):
+        super().__init__(number_of_x, f)
+        self.giving_table = giving_table
+
+    def compare_with_table(self, table, fs):
+        # First study - find not match parts
+
+        compare_table = []
+        compare_f = []
+        for row in self.giving_table:
+            for i in range(2 ** self.n):
+                # if we get incorrect answer we don't  compare all Vars
+                if row[-1] == fs[i]:
+                    table_r = [t[i] for t in table]
+                    if table_r.count(1) >= row[0:-1].count(1) and \
+                            table_r.count(0) >= row[0:-1].count(0):
+                        if table_r not in compare_table:
+                            compare_table.append(table_r)
+                            compare_f.append(fs[i])
+        return compare_table, compare_f
+
+    def print_compare_table(self, com_table, com_f):
+        print(self.create_title(self.n))
+        form = '{}   ' * (self.n + 1)
+        for i in range(len(com_f)):
+            print(form.format(*com_table[i], com_f[i]))
+
+    def solve(self):
+        table = self.create_var_table(self.n)
+        fs = self.create_log_table()
+        print('Full Vars Table')
+        self.print_log_table(self.n, table, fs)
+        if self.giving_table is not None:
+            print('\nFind all matches\n')
+            ct, cf = self.compare_with_table(table, fs)
+            self.print_compare_table(ct, cf)
+
+
+# EXAMPLES
+def lts_test():
+    n = 4
+    solver = BaseLTS(n)
+    solver.solve()
+
+
+def formul_lts_test():
+    f = lambda x, y, z: (((not x) and y) or x) and z
+    formul = OneFormulaLTS(3, f)
+    formul.solve()
+
+
+def lot_lts_test():
+    table = [[1, 0, 0],
+             [1, 0, 1],
+             [1, 1, 0]]
+    f = lambda x, y, z: ((not x) or z) and y
+    f2 = lambda x, y, z: (x and not y) or z
+    f4 = lambda x, y, z: (not x or y) or not z
+    f3 = lambda x, y, z: (x and y) and not z
+    more_formul = LotFormulaLTS(3, [f,f2,f3,f4], giving_table=table, pattern=[1, 0, 1])
+    more_formul.solve()
+
+
+def finder_lts_test():
+    # First with gen args
+
+    table = BaseLTS.create_var_table(2)
+    # finder = FinderLTS(2, arrow, table)
+    # finder.solve()
+    # Second with giving table
+    table = [
+        [None, None, 1],
+        [None, 1, 1],
+        [0, None, 1],
+        [1, 0, 0],
+    ]
+    # finder = FinderLTS(2, arrow, table)
+    # finder.solve()
+
+
+def main():
+    # lts_test()
+    # formul_lts_test()
+    lot_lts_test()
+    # finder_lts_test()
+
+
+if __name__ == '__main__':
+    main()
